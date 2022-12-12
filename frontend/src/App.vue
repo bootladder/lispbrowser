11609b3b024fc2bd37d68a15d95d6534c354aabf
Steve Anderson <steve@bootladder.com>
2022-12-12 03:01:10 -0500

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+
+import {onMounted,ref, watch} from 'vue/dist/vue.esm-bundler.js';
+
+    
+export default {
+	components: { },
+	setup () {
+		return {
+		}
+	},
+
+
+  watch: {
+    "env.thedatamodel": { async handler(newr, oldr) {
+            }, deep:true,
+        },
+      },
+
+	methods: {
+	},
+
+	computed: {
+	},
+
+
+  async mounted() {
+  }
+}
+
+</script>
+
+<template>
+
+    <div class="bg-red-100">
+        wat
+    </div>
+</template>
+

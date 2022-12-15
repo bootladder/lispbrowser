9f1630000d02d1b8b4cb79460ca1c464966aa48e
Steve Anderson <steve@bootladder.com>
2022-12-15 10:00:23 -0500

    functable and sexpr

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,42 @@
 <script>
+import {sexpr} from './sexpr.js'
 
 import {onMounted,ref, watch} from 'vue/dist/vue.esm-bundler.js';
 
+const expr1   = ref('(concat "hello world" (concat "other"))')
+const expr2   = ref('(add 1 1 1)')
+const expr    = ref('(add 1 1 (add 1 1) )')
+//const expr = ref('(concat "hello world")')
+
+const functable = {
+    concat: (args) => { return args.join("")},
+    add: (args) => { var acc = 0; args.forEach(a => {acc = acc + a}); return acc }
+}
+
+const awesomeeval = (expr) => {
+
+    if(false == isNaN( parseInt(expr) ))
+        return parseInt(expr)
+
+    const tokens = expr
+    const func = functable[tokens[0]]
+    const args = tokens.slice(1)
+    const evalargs = args.map( arg => awesomeeval(arg))
+    const result = func(evalargs)
+    return result
+}
+
+
+const awesomeparse = (expr) => {
+    return JSON.parse(expr)
+}
     
 export default {
 	components: { },
 	setup () {
 		return {
+            expr:expr,
+            sexpr:sexpr
 		}
 	},
 
@@ -18,9 +48,16 @@ export default {
       },
 
 	methods: {
+        awesomeeval: function(expr){
+            //const parsed = awesomeparse(expr)
+            const parsed = sexpr(expr)[0]
+            return awesomeeval(parsed)
+            //return parsed
+        }
 	},
 
 	computed: {
+            parsed : function() {return this.sexpr(this.expr)}
 	},
 
 
@@ -34,6 +71,15 @@ export default {
 
     <div class="bg-red-100">
         wat
+    </div>
+    <div class="bg-red-100">
+        EXPR: {{expr}}
+    </div>
+    <div class="bg-red-100">
+        Parsed: {{parsed}}
+    </div>
+    <div class="bg-red-100">
+        Eval: {{awesomeeval(expr)}}
     </div>
 </template>
 
